97d34f58ce4f900af12d27dbdb4c719c7e50ae97
ΛXXӨ <mr.alto2008@gmail.com>
2026-08-31 21:20:00 +0530

    Update update_readme.py

--- .github/scripts/update_readme.py
+++ .github/scripts/update_readme.py
@@ -479,7 +479,7 @@ def main():
 <details>
 <summary><b>Profile Overview</b></summary>
 <br>
-I am Althaf (Axxo), a technology enthusiast from Sri Lanka (🇱🇰), bridging the gap between innovative web design and offensive cybersecurity. My world revolves around the code that builds the web and the techniques that secure it. I specialize in full-stack applications, red team operations, penetration testing, and automation.Check my Linked in profile - https://www.linkedin.com/in/axxo
+I am Althaf (Axxo), a technology enthusiast from Sri Lanka (🇱🇰), bridging the gap between innovative web design and offensive cybersecurity. My world revolves around the code that builds the web and the techniques that secure it. I specialize in full-stack applications, red team operations, penetration testing, and automation. Check my Linked in profile - https://www.linkedin.com/in/axxo
 
 <br><br>
 <a href="https://tryhackme.com/p/alto.hacked">
